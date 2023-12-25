bf401679987031077bf27f624d1ed86ada281287
Michael Way <97139664+mjsway@users.noreply.github.com>
2023-12-25 08:05:07 -0500

    completed quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (num_of_apples: u8) -> u8 {
+    if num_of_apples <= 40 {
+        return num_of_apples * 2;
+    } else {
+        return num_of_apples;
+    }
+}
 
 // Don't modify this function!
 #[test]
